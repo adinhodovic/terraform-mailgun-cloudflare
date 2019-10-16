89aebb8198b3dde29acd4b11e9ab783a60f5808f
Adin Hodovic <hodovicadin@gmail.com>
2019-10-16 23:20:15 +0200

    Use zone_id in examples

--- examples/root-example/main.tf
+++ examples/root-example/main.tf
@@ -1,8 +1,8 @@
 module "mailgun_cloudflare" {
   source = "github.com/adinhodovic/terraform-mailgun-cloudflare"
 
-  mailgun_domain  = "mg.example.com"
-  cloudflare_zone = "example.com"
+  mailgun_domain = "mg.example.com"
+  zone_id        = "my-zone"
   # Can't exceed 32 chars.
   smtp_password = "very_secret"
   spam_action   = "tag"
