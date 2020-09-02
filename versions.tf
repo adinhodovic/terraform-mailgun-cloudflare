7c6af3efd2c99c7928f0f3f6353ba7372da9cff3
Adin Hodovic <hodovicadin@gmail.com>
2020-09-03 00:38:52 +0200

    Remove mailgun v3 module

--- versions.tf
+++ versions.tf
@@ -3,17 +3,8 @@ terraform {
     cloudflare = {
       source = "terraform-providers/cloudflare"
     }
-    mailgunv3 = {
-      # TF-UPGRADE-TODO
-      #
-      # No source detected for this provider. You must add a source address
-      # in the following format:
-      #
-      # source = "your-registry.example.com/organization/mailgunv3"
-      #
-      # For more information, see the provider source documentation:
-      #
-      # https://www.terraform.io/docs/configuration/providers.html#provider-source
+    mailgun = {
+      source = "terraform-providers/mailgun"
     }
   }
   required_version = ">= 0.13"
