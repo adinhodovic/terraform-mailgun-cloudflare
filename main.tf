71ef6e603d67c6cb5d7200609e37fe2b4445597d
adinhodovic <hodovicadin@gmail.com>
2024-01-10 18:47:44 +0100

    fix: Add priority to receiving records

--- main.tf
+++ main.tf
@@ -15,16 +15,15 @@ resource "cloudflare_record" "mailgun_sending_records" {
   name    = lookup(mailgun_domain.default.sending_records[count.index], "name")
   value   = lookup(mailgun_domain.default.sending_records[count.index], "value")
   type    = lookup(mailgun_domain.default.sending_records[count.index], "record_type")
-  ttl     = 1
   proxied = false
 }
 
 resource "cloudflare_record" "mailgun_receiving_records" {
-  count   = 2
-  zone_id = var.zone_id
-  name    = var.mailgun_domain
-  value   = lookup(mailgun_domain.default.receiving_records[count.index], "value")
-  type    = lookup(mailgun_domain.default.receiving_records[count.index], "record_type")
-  ttl     = 1
-  proxied = false
+  count    = 2
+  zone_id  = var.zone_id
+  name     = var.mailgun_domain
+  value    = lookup(mailgun_domain.default.receiving_records[count.index], "value")
+  type     = lookup(mailgun_domain.default.receiving_records[count.index], "record_type")
+  priority = lookup(mailgun_domain.default.receiving_records[count.index], "priority")
+  proxied  = false
 }
