2e68b127cd32d8265f49ec72cd75f6f74ea1ec2a
adinhodovic <hodovicadin@gmail.com>
2026-07-03 14:18:59 +0200

    fix: keep paths static

--- main.tf
+++ main.tf
@@ -9,12 +9,25 @@ resource "mailgun_domain" "default" {
   }
 }
 
+locals {
+  sending_records = concat(
+    [for record in mailgun_domain.default.sending_records_set : record if record.record_type == "TXT" && length(regexall("_domainkey", record.name)) == 0],
+    [for record in mailgun_domain.default.sending_records_set : record if record.record_type == "TXT" && length(regexall("_domainkey", record.name)) > 0],
+    [for record in mailgun_domain.default.sending_records_set : record if record.record_type == "CNAME"],
+  )
+
+  receiving_records = concat(
+    [for record in mailgun_domain.default.receiving_records_set : record if record.value == "mxa.mailgun.org"],
+    [for record in mailgun_domain.default.receiving_records_set : record if record.value == "mxb.mailgun.org"],
+  )
+}
+
 resource "cloudflare_dns_record" "mailgun_sending_records" {
   count   = 3
   zone_id = var.zone_id
-  name    = tolist(mailgun_domain.default.sending_records_set)[count.index].name
-  content = tolist(mailgun_domain.default.sending_records_set)[count.index].value
-  type    = tolist(mailgun_domain.default.sending_records_set)[count.index].record_type
+  name    = local.sending_records[count.index].name
+  content = local.sending_records[count.index].value
+  type    = local.sending_records[count.index].record_type
   proxied = false
   ttl     = 1
 }
@@ -23,9 +36,9 @@ resource "cloudflare_dns_record" "mailgun_receiving_records" {
   count    = 2
   zone_id  = var.zone_id
   name     = var.mailgun_domain
-  content  = tolist(mailgun_domain.default.receiving_records_set)[count.index].value
-  type     = tolist(mailgun_domain.default.receiving_records_set)[count.index].record_type
-  priority = tolist(mailgun_domain.default.receiving_records_set)[count.index].priority
+  content  = local.receiving_records[count.index].value
+  type     = local.receiving_records[count.index].record_type
+  priority = local.receiving_records[count.index].priority
   proxied  = false
   ttl      = 1
 }
